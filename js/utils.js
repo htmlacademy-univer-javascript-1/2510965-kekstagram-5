3aa1cbedafdab7268308135fae4bd9877754dbfa
Ktshka <151648756+Ktshka@users.noreply.github.com>
2024-10-27 16:51:38 +0500

    add modules

---
+++ js/utils.js
@@ -0,0 +1,8 @@
+export function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  export function getRandomElement(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
