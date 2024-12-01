18d3bb29181303b8c33d68170090486d1e8e7531
Ktshka <151648756+Ktshka@users.noreply.github.com>
2024-12-01 17:53:59 +0500

    comments and photos

---
+++ js/utils.js
@@ -0,0 +1,11 @@
+export function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  export function getRandomElement(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+  
+  const isEscKey = (evt) => evt.key === 'Escape';
+  
+  export {isEscKey};
