bfa22c20590e14185bce3b79f37dd40439272f75
Ktshka <151648756+Ktshka@users.noreply.github.com>
2025-01-02 23:01:17 +0500

    mod 11 task 1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
-import PictureRenderer from './pictureRenderer.js';
+import './pictures.js';
+import {loadData} from './fetch.js';
+import {renderPhotos} from './uploadPhoto.js';
+import './addHashtag.js';
+import './slider-and-control.js';
 
-const pictures = [
-  {
-    url: './photos/1.jpg',
-    description: 'Описание изображения 1',
-    likes: 10,
-    comments: 5
-  },
-  {
-    url: './photos/2.jpg',
-    description: 'Описание изображения 2',
-    likes: 20,
-    comments: 10
-  },
-];
+let photos;
 
-const renderer = new PictureRenderer('.pictures', '#picture');
-renderer.renderPictures(pictures);
+const onSuccess = (data) => {
+  photos = data.slice();
+  renderPhotos(data.slice());
+  document.querySelector('.img-filters').classList.remove('img-filters--inactive');
+};
+
+const onFail = () => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolue';
+  messageAlert.style.left = 0;
+  messageAlert.style.top = 0;
+  messageAlert.style.right = 0;
+  messageAlert.style.fontSize = '30px';
+  messageAlert.style.backgroundColor = 'red';
+  messageAlert.style.textAlign = 'center';
+  messageAlert.textContent = 'Возникла ошибка при загрузке фотографий';
+  document.body.append(messageAlert);
+};
+
+loadData(onSuccess, onFail);
+
+export {photos};
